4e4b5b266d49e39bc7de16f971f05c1cd74cf696
gustos <jucie15@nate.com>
2017-06-15 17:53:03 +0900

    파일 네이밍 수정

--- piggybook/cast/views.py
+++ piggybook/cast/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'cast/PBMain.html')
+    return render(request, 'cast/index.html')
