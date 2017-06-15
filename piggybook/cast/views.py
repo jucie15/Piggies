6ef2ce32ba2aff6969accf1b83dd19101305de98
SungHwanYoon <sparkupnova@gmail.com>
2017-06-15 16:28:26 +0900

    Template 분리. Layout과 MainContent로 분리.

--- piggybook/cast/views.py
+++ piggybook/cast/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'cast/piggyBookMain.html')
+    return render(request, 'cast/PBMain.html')
