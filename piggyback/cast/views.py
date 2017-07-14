6ab295b52faa5c3020628586e26a7bc06a12547a
gustos <jucie15@nate.com>
2017-07-14 15:42:57 +0900

    컨텐츠 감정표현 ajax 처리

--- piggyback/cast/views.py
+++ piggyback/cast/views.py
@@ -98,9 +98,11 @@ def contents_emotion(request, contents_pk):
         user = request.user # 현재 유저의 정보를 받아온다.
         contents = get_object_or_404(Contents, pk=contents_pk) # 현재 콘텐츠 인스턴스 생성
         emotion_name = request.GET.get('emotion_name','') # url GET 정보에 담겨있는 감정정보를 받아온다.
+        before_emotion_name = ''
         if user.contents_emotion_set.filter(contents=contents).exists():
             # 해당 컨텐츠에 감정 표현을 이미 해놓은 경우
             user_emotion = user.contents_emotion_set.get(contents=contents) # 해당유저가 컨텐츠에 해놓은 감정표현을 정보를 받아와
+            before_emotion_name = user_emotion.name
             if user_emotion.name == emotion_name:
                 # 같은 감정을 한번 더누르면 삭제.
                 ContentsEmotion.objects.filter(contents=contents, user=user).delete() # 인스턴스 삭제
@@ -115,9 +117,13 @@ def contents_emotion(request, contents_pk):
                 contents=contents,
                 name=emotion_name,
             )
+        emotion_count = ContentsEmotion.objects.filter(contents=contents, name=emotion_name).count()
+        before_emotion_count = ContentsEmotion.objects.filter(contents=contents, name=before_emotion_name).count()
         context = {}
         context['status'] = 'success'
-
+        context['emotion_count'] = emotion_count
+        context['before_emotion_name'] = before_emotion_name
+        context['before_emotion_count'] = before_emotion_count
     else:
         context = {}
         context['message'] = '잘못된 접근입니다.'
