31cec3e2a3bc20d5030753240d0ef0fb07d50d35
SungHwanYoon <sparkupnova@gmail.com>
2017-07-18 17:54:56 +0900

    grid안에 tagbox 조정. 수정 필요.

--- piggyback/cast/static/cast/js/app.js
+++ piggyback/cast/static/cast/js/app.js
@@ -1,28 +1,29 @@
 $(document).ready(function() {
     // show sidebar and overlay
+
     function showSidebar() {
         sidebar.css('margin-left', '0');
 
         overlay.show(0, function() {
             overlay.fadeTo('500', 0.5);
-        
+
         //sidebar 그림자 생성
         sidebar.css('box-shadow', '10px 0px 10px 0 rgba(0,0,0,0.5)');
-            
-        });   
+
+        });
     }
 
     // hide sidebar and overlay
     function hideSidebar() {
         sidebar.css('margin-left', sidebar.width() * -1 + 'px');
-        
+
         //sidebar 그림자 삭제
         sidebar.css('box-shadow', '0px 0px 0px 0 rgba(0,0,0,0)');
 
         overlay.fadeTo('500', 0, function() {
             overlay.hide();
         });;
-        
+
     }
 
     // selectors
@@ -56,3 +57,4 @@ $(document).ready(function() {
         hideSidebar();
     });
 });
+
