fefd25355778a725f601670eadef9bda5a29422a
gustos <jucie15@nate.com>
2017-09-26 15:48:30 +0900

    contents_db_create update youtube 크롤링 수정

--- piggyback/cast/utils.py
+++ piggyback/cast/utils.py
@@ -14,7 +14,7 @@ ROOT = lambda *args: os.path.join(settings.BASE_DIR, 'cast', 'static', 'cast', '
 def contents_db_create():
     # youtube 영상을 크롤링함과 동시에 디비에 저장
 
-    driver = webdriver.Firefox() # 파이어폭스를 불러온다.
+    driver = webdriver.Chrome('/Users/gustos/Downloads/chromedriver') # 크롬 드라이버 사용
 
     with open(ROOT('crawling_list.txt'), 'rt') as f:
         keyword_list = f.read().split('\n')[:-1] # 파일에서 키워드 리스트를 받아온다
@@ -27,19 +27,23 @@ def contents_db_create():
         list_html = driver.page_source # 접속한 페이지의 소스를 받아온다,
         list_soup = BeautifulSoup(list_html, 'html.parser') # html_parser 생성
 
-        for a_tag in list_soup.select('ytd-thumbnail a[href*=watch]'):
+        for a_tag in list_soup.select('ytd-thumbnail a[href^=/watch]'):
             # 영상리스트 페이지에서 각 영상들의 url을 받아오기 위해 a태그를 받아온다 검색결과에 영상뿐아니라 다른 결과물도 있어 href속성에 watch가 들어가있는 태그만 불러온다..
 
             watch_url = 'https://www.youtube.com' + a_tag['href'] # a태그의 href를 통해 url 파싱
 
             driver.get(watch_url) # 영상 url로 접속
+            condition = True
+            while condition:
+                try:
+                    contents_title = driver.find_element_by_css_selector('h1.title').text
+                    contents_desc = driver.find_element_by_css_selector('#description').text
+                    condition = False
+                except Exception as e:
+                    print(e)
+                    time.sleep(3)
 
-            watch_html = driver.page_source
-            watch_soup = BeautifulSoup(watch_html, 'html.parser') # 각 영상 페이지의 html_parser 생성
-
-            contents_title = watch_soup.select('.watch-title-container span')[0]['title'] # 해당 페이지에서 제목을 받아온다.
-            contents_desc = watch_soup.select('#watch-description-text p')[0].text # 해당 페이지에서 설명글을 받아온다.
-            contents_embed_url = 'https://www.youtube.com/embed/' + a_tag['href'].split('=')[-1] # 해당 페이지의 url을 통해 embed url을 만들어낸다.
+            contents_embed_url = 'https://www.youtube.com/embed/' + a_tag['href'].split('=')[-1]
 
             if not Contents.objects.filter(title=contents_title).exists():
                 # DB에 해당 글이 없을 경우 저장
@@ -49,7 +53,6 @@ def contents_db_create():
                 contents.description = contents_desc
                 contents.url_path = contents_embed_url
                 contents.save()
-
 
 def congressman_db_create():
     # 크롤링 해놓은 데이터를 불러와 디비에 저장
